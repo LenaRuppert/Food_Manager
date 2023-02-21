50f42ee1561d144686777a40a6c69319171fdce4
lenaruppert <l.zi@gmx.de>
2023-02-21 19:23:14 +0100

    create method listFood with Tests

---
+++ backend/src/test/java/com/example/backend/controller/FoodControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FoodControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    void listFood() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/food"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+}
